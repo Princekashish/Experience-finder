4d5a5623de96dd1431671a60a1cbecdcb9622cc5
Prince <princekashishbbu@gmail.com>
2024-10-13 15:43:41 +0530

    PWA

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,56 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import { VitePWA } from "vite-plugin-pwa";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: "autoUpdate",
+      includeAssets: ["favicon.ico", "masked-icon.png", "apple-touch-icon.png"],
+      manifest: {
+        name: "Finder App",
+        short_name: "Finder",
+        description: "Experience finder",
+        theme_color: "#ffffff",
+        icons: [
+          {
+            src: "logo192.png",
+            sizes: "192x192",
+            type: "image/png",
+          },
+          {
+            src: "logo512.png",
+            sizes: "512x512",
+            type: "image/png",
+            purpose: "maskable",
+          },
+          {
+            src: "logo180.png",
+            sizes: "180x180",
+            type: "image/png",
+          },
+        ],
+        screenshots: [
+          {
+            src: "/screenshot-desktop.png",
+            sizes: "1280x720",
+            type: "image/png",
+            form_factor: "wide",
+          },
+          {
+            src: "/screenshot-mobile.png",
+            sizes: "720x1280",
+            type: "image/png",
+            form_factor: "narrow",
+          },
+        ],
+      },
+      devOptions: {
+        enabled: true,
+        type: "module",
+      },
+    }),
+  ],
+});
